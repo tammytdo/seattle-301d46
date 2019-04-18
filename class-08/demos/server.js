ebf09d9c6d64a0d76f3dc4d04b8388c11474ae85
Nicholas Carignan <nick.carignan@sbcglobal.net>
2019-04-17 17:40:50 -0700

    day 8 demo

--- class-08/demos/server.js
+++ class-08/demos/server.js
@@ -7,12 +7,17 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000 ;
 const express = require('express');
 const cors = require('cors');
-const superagent = require('superagent');
+const superagent = require('superagent'); // talks to the web
+const pg = require('pg'); // postgres, talks to psql
+
+// postgres client setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', error => console.error(error))
 
 // server definition
 const app = express();
 app.use(cors());
-
 
 app.get('/location', searchLatLng);
 
@@ -42,18 +47,40 @@ function DailyWeather(rawDayObj){
 function searchLatLng(request, response){
   // Take the data from the front end ('Seattle')
   const searchQuery = request.query.data;
+  //check if it has been looked for before
+  console.log('checking the DATABASE');
+  client.query('SELECT * FROM locations WHERE search_query=$1', [searchQuery])
+    .then(result => {
+      console.log('result from DATABASE');
+      if (result.rows.length) { // (stuff in the db)
+        console.log('Exists in the DATABASE');
+        response.send(result.rows[0])
+      } else { // I've never searched that before
+        getStuffFromGoogle(searchQuery, response)
+
+      }
+    })
+
+  // ask superagent to go get data from google, if I don't have the data muself
+
+}
+
+function getStuffFromGoogle(searchQuery, response){
   // Compose the url using our secret api key and the search term
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`;
-
-  // ask superagent to go get data from google
+  // go get the data from google
   superagent.get(url).then(result => {
     // take the front end query and the results from google and normalize the data
     const location = new Location(searchQuery, result.body.results[0]);
     console.log('From Google API: \n', location);
     // send the location to the front end
     response.send(location);
+
+    console.log('Not in the database, inserting into DATABASE');
+    client.query(`INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4)`, [location.search_query, location.formatted_query, location.latitude, location.longitude]);
   })
 }
+
 
 function getWeather (request, response) {
   console.log('from the front end: \n', request.query.data);
